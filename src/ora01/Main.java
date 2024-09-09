789c58d501bf3b4182953345a4c6654251869b19
DMIlan05 <dmilan.felvi@gmail.com>
2024-09-09 10:36:13 +0200

    A körtéből alma lett

--- src/ora01/Main.java
+++ src/ora01/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        System.out.println("körte");
+        System.out.println("alma");
         bubbleSort();
     }
 }
